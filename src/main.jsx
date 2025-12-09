5c3de2df7d4846f48c38f2252e177d0efdafe7b8
MUKIL KUMAR <42886243+bukil@users.noreply.github.com>
2025-12-09 12:36:55 +0530

    ghj
    ghj

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme/theme'
 import './index.css'
 import App from './App.jsx'
 
-// Get the base name from vite config (it was set to /crm/)
-// In development it might be '/', in production it will be '/crm/'
-const basename = import.meta.env.BASE_URL;
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
